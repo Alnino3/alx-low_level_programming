8f6ce1290c8d52dd9119ef20877ac42ba0e0c78c
Alnino3 <107004617+Alnino3@users.noreply.github.com>
2022-07-08 19:36:19 -0700

    Update 2_largest_number.c

--- 2_largest_number.c
+++ 2_largest_number.c
@@ -13,19 +13,40 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a > b)
     {
-        largest = a;
-    }
-    else if (b > a && a > c)
+        if(b>c)
+        {
+            largest=a;
+        }
+        else
+        {
+            if(c>a)
+            {
+                largest=c;
+            }
+            else
+            {
+                largest=a;
+            }
+    else
     {
-        largest = b;
+        if(b>c)
+        {
+            largest=b;
+        }
+        else
+        {
+            if(c>b)
+            {
+                larges=c;
+            }
+            else
+            {
+                largest=b;
+            }
+        }
     }
-    else if(c > a && a>b)
-    {
-        largest = c;
+    return(largest)
     }
-    
-
-    return (largest);
-}
+           
